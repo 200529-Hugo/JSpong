8ed5de99efda7538f1538f2cc1c80201163bc8fc
200529-Hugo <78649357+200529-Hugo@users.noreply.github.com>
2021-04-13 13:54:45 +0200

    Delete webdev-pong2021-stap10 directory

--- webdev-pong2021-stap10/assets/js/player.js
+++
@@ -1,9 +0,0 @@
-class Player extends Rectangle
-{
-    constructor(x=0, y=0, id, color='white') {
-        super(x, y, 20, 100, color);
-        this.id = id;
-        this.color = color;
-        this.score = 0;
-    }
-}
